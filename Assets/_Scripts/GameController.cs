10938d6f41247bca4e2f5d611ebbe7002a6edef9
JessRudder <thejessicarudder@gmail.com>
2015-07-11 18:36:34 -0400

    Randomly generate coins on game start

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 	private GameObject player;
-	private GameObject coin;
+	public GameObject coin;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
-		coin = GameObject.FindGameObjectWithTag ("Coin");
+
 		this.transform.SetParent (player.transform);
+
+		for (var i = 0; i < 50; i++) {
+			Instantiate(coin, new Vector3 (Random.Range (-96f,96f),2,Random.Range (-96f,96f)), Quaternion.identity);
+		}
 	}
 	
 	// Update is called once per frame
