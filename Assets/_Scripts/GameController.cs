224c4f41e91b483a91fb783f94d01124f0d7c3d0
JessRudder <thejessicarudder@gmail.com>
2015-07-11 18:18:51 -0400

    Parent camera to player. Move player with getaxis input

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 
 public class GameController : MonoBehaviour {
+	private GameObject player;
+	private GameObject coin;
 
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.FindGameObjectWithTag("Player");
+		coin = GameObject.FindGameObjectWithTag ("Coin");
+		this.transform.SetParent (player.transform);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/_Scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	private float horiz;
+	private float vert;
+
+	[Range(0,10)] public int speed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horiz = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		vert = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		transform.Translate (new Vector3 (horiz, 0, vert));
+
+	}
+}
