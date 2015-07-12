3f1d8fed10c5a2b3ec56e5ab0c890df4a6190fe2
JessRudder <thejessicarudder@gmail.com>
2015-07-11 20:19:24 -0400

    Fixed camera script

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 	private GameObject player;
 	public GameObject coin;
-
+	private Transform playerTransform;
+	private Vector3 cameraOrientationVector = new Vector3 (0, 0, -10);
+	
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
-
-		this.transform.SetParent (player.transform);
+		playerTransform = player.transform;
+	
 
 		for (var i = 0; i < 50; i++) {
 			Instantiate(coin, new Vector3 (Random.Range (-96f,96f),2,Random.Range (-96f,96f)), Quaternion.identity);
@@ -18,6 +20,6 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.position = playerTransform.position + cameraOrientationVector;
 	}
 }
--- Assets/_Scripts/PlayerScript.cs
+++ Assets/_Scripts/PlayerScript.cs
@@ -5,19 +5,22 @@ public class PlayerScript : MonoBehaviour {
 
 	private float horiz;
 	private float vert;
+	private Rigidbody rb;
 
-	[Range(0,10)] public int speed;
+	[Range(400,900)] public int speed;
 
 	// Use this for initialization
 	void Start () {
-
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		horiz = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 		vert = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-		transform.Translate (new Vector3 (horiz, 0, vert));
+//		transform.Translate (new Vector3 (horiz, 0, vert));
+		rb.AddForce (Vector3.right * horiz);
+		rb.AddForce (Vector3.forward * vert);
 
 	}
 }
